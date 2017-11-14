48eb7296ed607c58ae12db421a8d1e44bb0ac6d7
fengzq <215836017@qq.com>
2017-11-14 21:56:17 +0800

    修改了小数点问题，并且调试和修改了退格等其他的情况下的bug，目前算是单目运算较好的代码了，对于ios入门demo来说，后面可能不再有版本更新了

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,18 +8,16 @@
 
 import UIKit
 
-/**
- 代码参考链接：
-http://blog.csdn.net/qq_29284809/article/details/50803536
-http://blog.csdn.net/tujiaw/article/details/44132419
- */
 class ViewController: UIViewController {
    
     var num01 : Float = 0.0
     var num02 : Float = 0.0
     var result : Float = 0.0
+    var strNum1 : String = ""
+    var strNum2 : String = ""
     
     var isFristInput = true
+    var isSecondInput = true
     var flagOpt = -1
     
     @IBOutlet var labelShowNum: UILabel!
@@ -40,34 +38,68 @@ class ViewController: UIViewController {
     //加法
     @IBAction func btnAdd(_ sender: Any) {
         flagOpt = 0
-        num01 = Float(labelShowNum.text!)!
-        labelShowNum.text = labelShowNum.text! + " + "
+        strNum1 = labelShowNum.text!
+        if strNum1 == ""{
+            return
+        }
+        if strNum1.last == "."{
+            strNum1.removeLast()
+        }
+        num01 = Float(strNum1)!
+        labelShowNum.text = strNum1 + " + "
     }
     
     //减法
     @IBAction func btnSubtraction(_ sender: Any) {
         flagOpt = 1
-        num01 = Float(labelShowNum.text!)!
-        labelShowNum.text = labelShowNum.text! + " - "
+        strNum1 = labelShowNum.text!
+        if strNum1 == ""{
+            return
+        }
+        if strNum1.last == "."{
+            strNum1.removeLast()
+        }
+        num01 = Float(strNum1)!
+       labelShowNum.text = strNum1 + " - "
     }
     
    
     //乘法
     @IBAction func btnMultiplication(_ sender: UIButton) {
        flagOpt = 2
-        num01 = Float(labelShowNum.text!)!
-        labelShowNum.text = labelShowNum.text! + " x "
+        strNum1 = labelShowNum.text!
+        if strNum1 == ""{
+            return
+        }
+        if strNum1.last == "."{
+            strNum1.removeLast()
+        }
+        num01 = Float(strNum1)!
+        labelShowNum.text = strNum1 + " x "
     }
     //除法
     @IBAction func btnDivision(_ sender: Any) {
         flagOpt = 3
-        num01 = Float(labelShowNum.text!)!
-        labelShowNum.text = labelShowNum.text! + " / "
+        strNum1 = labelShowNum.text!
+        if strNum1 == ""{
+            return
+        }
+        if strNum1.last == "."{
+            strNum1.removeLast()
+        }
+        num01 = Float(strNum1)!
+        labelShowNum.text = strNum1 + " / "
     }
     
     //等于号
     @IBAction func btnEqual(_ sender: Any) {
 
+        if strNum2.last == "."{
+            strNum2.removeLast()
+        }
+        
+        num02 = Float(strNum2)!
+        
         switch flagOpt {
         case 0:
             result = num01 + num02
@@ -76,8 +108,11 @@ class ViewController: UIViewController {
         case 2:
             result = num01 * num02
         case 3:
+            if num02 == 0{
+             print("除数不能为0")
+            }else{
             result = num01 / num02
-            
+            }
         default:
             print("Error!")
             return
@@ -89,6 +124,8 @@ class ViewController: UIViewController {
         result = 0.0
         isFristInput = true
         flagOpt = -1
+        strNum2 = ""
+        strNum1 = ""
         
     }
     
@@ -96,17 +133,28 @@ class ViewController: UIViewController {
     //退格
     @IBAction func btnBack(_ sender: Any) {
         var str : String = labelShowNum.text!
+        if str.count < 1 {
+            return
+        }
+        
         let ch = str.removeLast()
         if ch == "+" || ch == "-" || ch == "x" || ch == "/"{
             flagOpt = -1
+            str.removeLast()
         }
         labelShowNum.text = str
     }
     
     //清空
     @IBAction func btnClear(_ sender: Any) {
-        labelShowNum.text = String(0)
+        labelShowNum.text = "0"
+        num01 = 0.0
+        num02 = 0.0
+        result = 0.0
         isFristInput = true
+        flagOpt = -1
+        strNum2 = ""
+        strNum1 = ""
     }
     
     //获取数字
@@ -116,11 +164,42 @@ class ViewController: UIViewController {
             isFristInput = false
         }
         if flagOpt != -1{
-            num02 = num02 + Float((sender.titleLabel?.text)!)!
+            if strNum2 == "0"{  // 考虑到0.情况下点了退格键
+            strNum2 = (sender.titleLabel?.text)!
+            }else{
+                strNum2 = strNum2 + (sender.titleLabel?.text)!
+            }
         }
          labelShowNum.text = labelShowNum.text! + (sender.titleLabel?.text)!
        
     }
+    
+    
+    //小数点的点击事件
+    @IBAction func getPoint(_ sender: Any) {
+        if flagOpt == -1{
+            if isFristInput {
+                labelShowNum.text = "0."
+                isFristInput = false
+            }else{
+                labelShowNum.text = labelShowNum.text! + "."
+            }
+        }
+        
+        if flagOpt != -1 {
+            if strNum2.isEmpty{
+                strNum2 = "0."
+                 labelShowNum.text = labelShowNum.text! + "0."
+            }else{
+                strNum2 = strNum2 + "."
+                 labelShowNum.text = labelShowNum.text! + "."
+            }
+            
+           
+         
+        }
+    }
+    
     
     
 }
